b7362c0b39d397cf45058571a547502aec0d7578
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-02 12:46:57 -0600

    Password generated with random numerals only

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // initiate variables
   var pwdLength = 0; 
-  var charLC = false;
-  var charUP = false;
-  var charNum = false;
-  var charSpecial = false;
+  var generatedPWD = '';
 
   // Collect password length from the user
   // Require a number between 8 and 128
   do {
-    var pwdLength = prompt("Password Length: please select a number between 8 and 128.");
+    pwdLength = prompt("Password Length: please select a number between 8 and 128.");
   } while (!Number(pwdLength) || Number(pwdLength) < 8 || Number(pwdLength) > 128);
 
 
@@ -20,10 +17,16 @@ function generatePassword() {
   // Options: lowercase, uppercase, numerals and special characters
   var charLC = confirm("Would you like to include lowercase letters?");
   var charUP = confirm("Would you like to include uppercase letters?");
-  var charNum = confirm("Would you like to include numerals?");
+  var charNum = confirm("Would you like to include numbers?");
   var charSpecial = confirm("Would you like to include special characters?");
   
+  for(var i=0; i < pwdLength; i++) {
+    var newChar = '';
+    if (charNum) { newChar = Math.floor(Math.random() * 10); }
+    generatedPWD += newChar;
+  }
 
+  return generatedPWD;
   
 }
 
