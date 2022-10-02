d1b7c8ceef5892f63acc21654dbfceacc64694c5
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-02 12:19:45 -0600

    Added generatePassword function and set up length prompt.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // initiate variables
+  var pwdLength = 0; // between 8 and 128
+  const charTypes = []; // array - lower, upper, numeric, special
+
+  var pwdLength = prompt("How many characters should it be? (between 8 and 128)");
+  // console.log(Number(pwdLength));
+
+  if(Number(pwdLength)) {
+    console.log("That's a number.");
+  } else {
+    console.log("nope.");
+  }
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
