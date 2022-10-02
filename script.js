c4e9e3ae7ff8c83f7e6cb34ec22f2739e32851a4
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-02 13:07:32 -0600

    Error handling added for user declining all character types

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getCharCriteria() {
+  
+  var charLC = confirm("Would you like to include lowercase letters?");
+  var charUP = confirm("Would you like to include uppercase letters?");
+  var charNum = confirm("Would you like to include numbers?");
+  var charSpecial = confirm("Would you like to include special characters?");
+
+  return[charLC, charUP, charNum, charSpecial];
+}
+
 function generatePassword() {
   // initiate variables
   var pwdLength = 0; 
   var generatedPWD = '';
+  var charTypes = [];
 
   // Collect password length from the user
   // Require a number between 8 and 128
@@ -15,13 +26,19 @@ function generatePassword() {
 
   // Collect character criteria from the user
   // Options: lowercase, uppercase, numerals and special characters
-  var charLC = confirm("Would you like to include lowercase letters?");
-  var charUP = confirm("Would you like to include uppercase letters?");
-  var charNum = confirm("Would you like to include numbers?");
-  var charSpecial = confirm("Would you like to include special characters?");
+ charTypes = getCharCriteria();
+ console.log(charTypes);
+    
+  if(!charTypes[0] && !charTypes[1] && !charTypes[2] && !charTypes[3]) {
+    alert("At least one type of character must be selected. Please try again.");
+    charTypes = getCharCriteria();
+  }
   
+
   for(var i=0; i < pwdLength; i++) {
     var newChar = '';
+
+    var charType = Math.floor(Math.random() * 5);
     if (charNum) { newChar = Math.floor(Math.random() * 10); }
     generatedPWD += newChar;
   }
